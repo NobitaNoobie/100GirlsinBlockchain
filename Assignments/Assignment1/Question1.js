4f627b62f4ee1a6a5b019e2cf60ce401210cfd70
NobitaNoobie <91742427+NobitaNoobie@users.noreply.github.com>
2023-05-20 21:32:44 +0530

    Update Question1.js

--- Assignments/Assignment1/Question1.js
+++ Assignments/Assignment1/Question1.js
@@ -1,1 +1,8 @@
 
+for (let i = 1; i <= 100; i++) {
+  const output = (i % 3 === 0 && i % 5 === 0) ? "FizzBuzz" :
+                 (i % 3 === 0) ? "Fizz" :
+                 (i % 5 === 0) ? "Buzz" :
+                 i;
+  console.log(output);
+}
